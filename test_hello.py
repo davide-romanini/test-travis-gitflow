48d024d1dc64862fd5683bd0cf1b593100b90c2d
Davide Romanini <davide.romanini@gmail.com>
2019-09-25 23:54:13 +0200

    fix test error

--- test_hello.py
+++ test_hello.py
@@ -3,4 +3,5 @@ from hello import hello
 def test_hello():
     assert hello("David") == "Hello David, welcome"
     assert hello("John") == "Hello John, welcome"
-    assert hello("Matt") == "Hello Matt, elcome"
+    assert hello("Matt") == "Hello Matt, welcome"
+    assert hello("Joe") == "Hello Joe, welcome"
