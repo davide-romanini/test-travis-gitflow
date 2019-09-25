2864bc14b487496505e83c41fc99fa7fde2aafcf
Davide Romanini <davide.romanini@gmail.com>
2019-09-25 23:46:51 +0200

    some other tests

--- test_hello.py
+++ test_hello.py
@@ -3,3 +3,4 @@ from hello import hello
 def test_hello():
     assert hello("David") == "Hello David, welcome"
     assert hello("John") == "Hello John, welcome"
+    assert hello("Matt") == "Hello Matt, welcome"
