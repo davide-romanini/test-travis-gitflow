8ea691e5d9115cec2c0b88037f000246322bfc53
Davide Romanini <davide.romanini@gmail.com>
2019-09-27 01:57:33 +0200

    fix pip install

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+pip install -r requirements.txt
 pytest
 python setup.py sdist
--- build_win.sh
+++ build_win.sh
@@ -3,6 +3,6 @@ choco install -y python
 export PATH=/c/Python37:$PATH
 python -m venv venv
 source venv/Scripts/activate
-pip install -r < requirements.txt
+pip install -r requirements.txt
 pytest
 python setup.py sdist
